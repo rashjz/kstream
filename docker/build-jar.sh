1b3199ff504f3fb94cde39f18953272d13fd426f
Rashad <rashadjavad@gmail.com>
2019-08-21 20:51:28 +0200

    dockerfile test

--- docker/build-jar.sh
+++ docker/build-jar.sh
@@ -1,8 +1,13 @@
 #!/bin/sh
-
-apk add --update maven
-apk add --update  unzip
+echo setup maven
+apk add --update maven > /dev/null 2>&1
+apk add --update  unzip > /dev/null 2>&1
+echo getting git project
 wget -P /tmp/ 'https://github.com/rashjz/kstream/archive/master.zip' && \
 unzip /tmp/master.zip -d /tmp/
+echo building project
 mvn package -f /tmp/kstream-master/pom.xml
-ls -la /tmp/kstream-master/
+chmod 777 /tmp/kstream-master/target/kstream-0.0.1-SNAPSHOT.jar
+mv /tmp/kstream-master/target/kstream-0.0.1-SNAPSHOT.jar /tmp/kstream.jar > /dev/null 2>&1
+rm -rf /tmp/kstream-master/
+ls -la /tmp/
